96ede74a0c415ad91015959f466cdefbe934b0c7
denghe <denghe@hotmail.com>
2023-07-13 13:40:22 +0800

    writing...

--- 01_hello_world/hello_world.cpp
+++ 01_hello_world/hello_world.cpp
@@ -1,6 +1,25 @@
-extern "C" {
-	int asdf();
+#include "xx_coros.h"
+
+xx::Task<int64_t> func1(int64_t a) {
+    co_yield 0;
+    co_return a + a;
 }
+xx::Task<int64_t> func2(int64_t b) {
+    co_return co_await func1(b) + co_await func1(b);
+}
+
 int main() {
-  return asdf();
+    xx::EventTasks<int, xx::Data_r&> tasks;
+    auto s = "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"s;
+    tasks.AddLambda([&]()->xx::Task<>{
+        auto secs = xx::NowSteadyEpochSeconds();
+        int64_t n = 0;
+        for (int64_t i = 0; i < 10000000; ++i) {
+            n += co_await func2(i);
+        }
+        std::cout << "n = " << n << ", secs = " << xx::NowSteadyEpochSeconds(secs) << "\n";
+        std::cout << s << std::endl;
+    });
+    while(tasks());
+    return 0;
 }
