43370f7ea6f6a9997e2c768a15c7ba8c01814070
denghe <denghe@hotmail.com>
2023-07-18 20:30:41 +0800

    writing...

--- docs/quick_example.cpp
+++ docs/quick_example.cpp
@@ -1,11 +1,15 @@
 /*
 emcc -lembind -o quick_example.js quick_example.cpp
 
+emrun quick_example.html
+
 show console:
 enter browse console mode: ( chrome : Control-Shift-J ) ( firefox: Control-Shift-K )
 
 */
+
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
 
 using namespace emscripten;
 
@@ -19,12 +23,19 @@ std::string hi(std::string name) {
 
 struct Foo {
     std::string name;
+    emscripten::val cb;
     Foo(int n) {
         name = std::to_string(n);
     }
     void ClearName() { name.clear(); }
     std::string const& GetName() const { return name; }
     void SetName(std::string name) { this->name = std::move(name); }
+    void SetCB(emscripten::val cb_) {
+        this->cb = std::move(cb_);
+    }
+    void CallCB() {
+        this->cb();
+    }
 };
 
 EMSCRIPTEN_BINDINGS(my_module) {
@@ -33,5 +44,7 @@ EMSCRIPTEN_BINDINGS(my_module) {
     class_<Foo>("Foo")
             .constructor<int>()
             .function("ClearName", &Foo::ClearName)
+            .function("SetCB", &Foo::SetCB)
+            .function("CallCB", &Foo::CallCB)
             .property("name", &Foo::GetName, &Foo::SetName);
 }
