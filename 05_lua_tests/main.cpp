361fa2315f86e95885dfe97965c2fc311a918379
denghe <denghe@hotmail.com>
2023-07-21 12:01:33 +0800

    writing...

--- 05_lua_tests/main.cpp
+++ 05_lua_tests/main.cpp
@@ -8,14 +8,37 @@ int main() {
     LuaStateWithInt L;
     xl::MakeUserdataWeakTable(L);
     xl::RegisterBaseEnv(L);
-    xl::DoString(L, R"(
-
-local t = {}
-for j = 1, 100000 do
-    -- todo
-end
-print(t)
-
-)");
+    xl::DoFile(L, "/res/test1.lua");
     return 0;
 }
+
+
+//#include <xx_dict.h>
+//#include <xx_list.h>
+//int main() {
+//    auto secs = xx::NowEpochSeconds();
+//
+//    // lua msvc exe                         // 0.19
+//    // js                                   // 0.17
+//
+//    // lua                                  // 0.31     0.43 ( -sALLOW_MEMORY_GROWTH=1 )
+//
+//    //std::unordered_map<int, int> ints;    // 0.267
+//
+//    //xx::Dict<int, int> ints;              // 0.16
+//    //ints.Reserve(10000000);
+//
+//    //std::vector<int> ints;                // 0.015
+//    //ints.resize(10000000);
+//
+//    xx::List<int> ints;                     // 0.013
+//    ints.Resize(10000000);
+//
+//    int i = 0;
+//    for (; i < 10000000; ++i) {
+//        ints[i] = i;
+//    }
+//    std::cout << xx::NowEpochSeconds(secs) << std::endl;
+//    std::cout << ints[0] << ints[i/2] << std::endl;
+//    return 0;
+//}
---
+++ 05_lua_tests/res/test1.lua
@@ -0,0 +1,6 @@
+local secs = NowEpochSeconds()
+local t = {}
+for j = 1, 10000000 do
+    t[j] = j
+end
+print(NowEpochSeconds() - secs)
