9687e12db4c89f8393f03e1d89e941e65e709e86
denghe <denghe@hotmail.com>
2023-07-25 16:36:52 +0800

    writing...

--- 05_lua_tests/main.cpp
+++ 05_lua_tests/main.cpp
@@ -4,11 +4,89 @@ using LuaStateWithInt = xx::Lua::StateWithExtra<int>;
 #include <xx_lua_shared.h>
 namespace xl = xx::Lua;
 
+
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+
+EM_JS(void, PrintValHandle, (emscripten::EM_VAL h), {
+    console.log(Emval.toValue(h));
+});
+
+void PrintVal(emscripten::val const& v) {
+    PrintValHandle(v.as_handle());
+}
+
+
 int main() {
+    emscripten_run_script(R"(
+
+function Foo() {
+    console.log('js: Foo()');
+    this.name = 'foo';
+}
+Foo.prototype.GetName = function() {
+    console.log('js: GetName()');
+    return this.name;
+};
+Foo.prototype.SetName = function(name) {
+    console.log('js: SetName(name) ' + name);
+    this.name = name;
+};
+
+Bar = {};
+Bar.Add = function(a, b) {
+    console.log('js: Bar.Add(a, b)');
+    console.log('js: a = ' + a);
+    console.log('js: b = ' + b);
+    return a + b;
+};
+
+)");
+
     LuaStateWithInt L;
     xl::MakeUserdataWeakTable(L);
     xl::RegisterBaseEnv(L);
-    xl::DoFile(L, "/res/test1.lua");
+
+    xx_assert(lua_gettop(L) == 0);
+    lua_newtable(L);                                            // ..., t
+    xx_assert(lua_gettop(L) == 1);
+    xl::SetField(L, "_tablename_", "Bar");                            // store tablename
+    lua_newtable(L);                                            // ..., t, mt
+    xx_assert(lua_gettop(L) == 2);
+    xl::SetFieldCClosure(L, "__index", [](auto L)->int{
+        lua_getfield(L, 1, "_tablename_");                                          // t, k, tablename
+        xx_assert(lua_type(L, -1) == LUA_TSTRING);
+        // todo: find k in tablename ??
+        lua_pushcclosure(L, [](lua_State*L)->int{
+            auto member = xl::To<std::string_view>(L, lua_upvalueindex(1));
+            auto tablename = xl::To<std::string_view>(L, lua_upvalueindex(2));
+            std::cout << "lua: call " << tablename << "." << member << std::endl;
+            // todo: forward send arguments?
+            auto a1 = xl::To<int>(L, 1);
+            auto a2 = xl::To<int>(L, 2);
+            auto r = emscripten::val::global(tablename.data()).call<emscripten::val>(member.data(), a1, a2);
+            // todo: switch case r type? convert to lua value?
+            return xl::Push(L, r.as<int>());
+        }, 2);                                                                      // t, c
+        return 1;
+    });
+    xx_assert(lua_gettop(L) == 2);
+    lua_setmetatable(L, -2);                                    // ..., t
+    xx_assert(lua_gettop(L) == 1);
+    lua_setglobal(L, "Bar");                                    // ...
+    xx_assert(lua_gettop(L) == 0);
+
+    xl::DoString(L, R"(
+
+print(Bar)
+print(Bar.Add)
+print(Bar.Add(1, 2))
+
+)");
+
+//    xl::DoFile(L, "/res/test1.lua");
     return 0;
 }
 
