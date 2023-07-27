4c84310954998b9907b9d0b1389f07c5a307d1ca
denghe <denghe@hotmail.com>
2023-07-27 18:41:51 +0800

    writing...

--- 05_lua_tests/main.cpp
+++ 05_lua_tests/main.cpp
@@ -21,9 +21,145 @@ void PrintVal(V const& v) {
     PrintValHandle(v.as_handle());
 }
 
+EM_JS(bool, IsFunction_, (emscripten::EM_VAL h), {
+    let tn = typeof(Emval.toValue(h));
+    console.log('js: IsFunction_ tn == ' + tn);
+    return tn == 'function';
+});
+
+bool IsFunction(V const& v) {
+    return IsFunction_(v.as_handle());
+}
+
+
 std::vector<V> gVals;
 std::vector<V*> gValptrs;
+std::unordered_map<std::string, double> gMap;
 // todo: cache metatable to registry
+
+void SetValMeta(lua_State* L);
+int HandleVal(lua_State* L, V& m);
+
+int PushValFunction(lua_State* L, V& m) {
+    new(lua_newuserdata(L, sizeof(V))) V(std::move(m));
+    lua_newtable(L);
+    xx::Lua::SetFieldCClosure(L, "__gc", [](lua_State* L)->int {
+        ((V*)lua_touserdata(L, 1))->~V();
+        return 0;
+    });
+    lua_setmetatable(L, -2);
+
+    lua_pushcclosure(L, [](lua_State*L)->int{
+        auto& f = *(V*)lua_touserdata(L, lua_upvalueindex(1));
+
+        auto n = lua_gettop(L);
+
+        gVals.resize(n);
+        gValptrs.resize(n);
+
+        for(int i = 0; i < n; ++i) {
+            auto idx = i + 1;
+            auto t = lua_type(L, idx);
+//                std::cout << "before switch t = " << t << std::endl;
+            switch(t) {
+                case LUA_TNIL:
+                    gVals[i] = V::null();
+                    gValptrs[i] = &gVals[i];
+                    break;
+                case LUA_TBOOLEAN:
+                    gVals[i] = V(xl::To<bool>(L, idx));
+                    gValptrs[i] = &gVals[i];
+                    break;
+                case LUA_TLIGHTUSERDATA:
+                    xx_assert(false);
+                case LUA_TNUMBER:
+                    gVals[i] = V(xl::To<double>(L, idx));
+                    gValptrs[i] = &gVals[i];
+                    break;
+                case LUA_TSTRING:
+                    gVals[i] = V(xl::To<char const*>(L, idx));
+                    gValptrs[i] = &gVals[i];
+                    break;
+                case LUA_TTABLE: {
+                    xl::To(L, idx, gMap);
+                    xx_assert(!gMap.empty());
+                    auto &o = gVals[i];
+                    gValptrs[i] = &o;
+                    o = V::object();
+                    for (auto &kv: gMap) {
+                        o.set(kv.first, kv.second);
+                    }
+                    gMap.clear();
+                    break;
+                }
+                case LUA_TFUNCTION:
+                    // todo: emscripten_run_script create global function with unique name, then val get it ? how to gc ?
+                    xx_assert(false);
+                case LUA_TUSERDATA:
+                    gValptrs[i] = (V*)lua_touserdata(L, idx);
+                    break;
+                case LUA_TTHREAD:
+                    xx_assert(false);
+                default:
+                    xx_assert(false);
+            }
+        }
+
+        V r;
+        switch(n) {
+            case 0:
+                r = f();
+                break;
+            case 1:
+                r = f(*gValptrs[0]);
+                break;
+            case 2:
+                r = f(*gValptrs[0], *gValptrs[1]);
+                break;
+            case 3:
+                r = f(*gValptrs[0], *gValptrs[1], *gValptrs[2]);
+                break;
+            case 4:
+                r = f(*gValptrs[0], *gValptrs[1], *gValptrs[2], *gValptrs[3]);
+                break;
+            case 5:
+                r = f(*gValptrs[0], *gValptrs[1], *gValptrs[2], *gValptrs[3], *gValptrs[4]);
+                break;
+                // known issue: add more ?
+            default:
+                xx_assert(false);
+        }
+        gVals.clear();
+
+        return HandleVal(L, r);
+    }, 1);
+
+    return 1;
+}
+
+int HandleVal(lua_State* L, V& m) {
+    if (m.isNull() || m.isUndefined()) return 0;
+    else if (m.isTrue()) return xl::Push(L, true);
+    else if (m.isFalse()) return xl::Push(L, false);
+    else if (m.isNumber()) {
+        auto v = m.template as<double>();
+        if ((int64_t)v == v) return xl::Push(L, (int64_t)v);
+        else return xl::Push(L, v);
+    }
+    else if (m.isString()) return xl::Push(L, m.template as<std::string>());
+    else if (IsFunction(m)) {
+        return PushValFunction(L, m);
+    }
+    else if (m.isArray()) {
+        // todo?
+        xx_assert(false);
+    } else {
+        new(lua_newuserdata(L, sizeof(V))) V(std::move(m));
+        SetValMeta(L);
+    }
+    return 1;
+}
+
 void SetValMeta(lua_State* L) {
     auto top = lua_gettop(L);
     xx_assert(top >= 1);
@@ -36,99 +172,13 @@ void SetValMeta(lua_State* L) {
     });
 
     xl::SetFieldCClosure(L, "__index", [](auto L)->int{
-        // known issue: verify member name exists maybe better?
-        lua_pushcclosure(L, [](lua_State*L)->int{
-            auto p = (V*)lua_touserdata(L, lua_upvalueindex(1));
-            auto memberName = xl::To<char const*>(L, lua_upvalueindex(2));
+        auto p = (V*)lua_touserdata(L, 1);
+        auto memberName = xl::To<char const*>(L, 2);
 
-            auto n = lua_gettop(L);
+        auto m = (*p)[memberName];
+        if (m.isNull() || m.isUndefined()) return 0;
 
-            gVals.resize(n);
-            gValptrs.resize(n);
-
-            for(int i = 0; i < n; ++i) {
-                auto t = lua_type(L, i + 1);
-//                std::cout << "before switch t = " << t << std::endl;
-                switch(t) {
-                    case LUA_TNIL:
-                        gVals[i] = V::null();
-                        gValptrs[i] = &gVals[i];
-                        break;
-                    case LUA_TBOOLEAN:
-                        gVals[i] = V(xl::To<bool>(L, i + 1));
-                        gValptrs[i] = &gVals[i];
-                        break;
-                    case LUA_TLIGHTUSERDATA:
-                        xx_assert(false);
-                    case LUA_TNUMBER:
-                        gVals[i] = V(xl::To<double>(L, i + 1));
-                        gValptrs[i] = &gVals[i];
-                        break;
-                    case LUA_TSTRING:
-                        gVals[i] = V(xl::To<char const*>(L, i + 1));
-                        gValptrs[i] = &gVals[i];
-                        break;
-                    case LUA_TTABLE:
-                        // todo: lua table to js Object ???
-                        xx_assert(false);
-                    case LUA_TFUNCTION:
-                        // todo: emscripten_run_script create global function with unique name, then val get it ? how to gc ?
-                        xx_assert(false);
-                    case LUA_TUSERDATA:
-                        gValptrs[i] = (V*)lua_touserdata(L, i + 1);
-                        break;
-                    case LUA_TTHREAD:
-                        xx_assert(false);
-                    default:
-                        xx_assert(false);
-                }
-            }
-
-//            std::cout << "before r = p->call" << std::endl;
-            V r;
-            switch(n) {
-                case 0:
-                    r = p->call<V>(memberName);
-                    break;
-                case 1:
-                    r = p->call<V>(memberName, *gValptrs[0]);
-                    break;
-                case 2:
-                    r = p->call<V>(memberName, *gValptrs[0], *gValptrs[1]);
-                    break;
-                case 3:
-                    r = p->call<V>(memberName, *gValptrs[0], *gValptrs[1], *gValptrs[2]);
-                    break;
-                case 4:
-                    r = p->call<V>(memberName, *gValptrs[0], *gValptrs[1], *gValptrs[2], *gValptrs[3]);
-                    break;
-                case 5:
-                    r = p->call<V>(memberName, *gValptrs[0], *gValptrs[1], *gValptrs[2], *gValptrs[3], *gValptrs[4]);
-                    break;
-                    // known issue: add more ?
-                default:
-                    xx_assert(false);
-            }
-            gVals.clear();
-//            std::cout << "after r = p->call" << std::endl;
-
-            if (r.isNull() || r.isUndefined()) return 0;
-            else if (r.isTrue()) return xl::Push(L, true);
-            else if (r.isFalse()) return xl::Push(L, false);
-            else if (r.isNumber()) return xl::Push(L, r.as<double>());
-            else if (r.isString()) return xl::Push(L, r.as<std::string>());
-            else if (r.isArray()) {
-                // todo
-                xx_assert(false);
-            } else {
-                new(lua_newuserdata(L, sizeof(V))) V(std::move(r));
-                SetValMeta(L);
-                return 1;
-            }
-
-            return 0;
-        }, 2);
-        return 1;
+        return HandleVal(L, m);
     });
 
     lua_setmetatable(L, -2);                                    // ..., ud
@@ -137,6 +187,15 @@ void SetValMeta(lua_State* L) {
 
 int main() {
     emscripten_run_script(R"(
+
+a = function() {};
+b = new Object;
+c = { x:1, y:2 };
+d = [ 1, 2 ];
+console.log( typeof(a) )
+console.log( typeof(b) )
+console.log( typeof(c) )
+console.log( typeof(d) )
 
 Bar = {};
 Bar.Add = function(a, b) {
@@ -148,6 +207,8 @@ Bar.CreateObject = function( name ) {
 Bar.Log = function( o ) {
     console.log( o );
 };
+
+p = { x:1, y:2 };
 
 )");
 
@@ -164,10 +225,21 @@ Bar.Log = function( o ) {
     xl::DoString(L, R"(
 local b = GetGlobalJsObj( "Bar" )
 print( b )
-print( b.Add(1, 2) )
+local f =  b.Add
+print( f )
+--[[
+b.Add = function()
+    print( "new func" )
+end
+]]
+print( f(1, 2) )
+
 local o = b.CreateObject( "asdf" )
 print( o )
 b.Log( o )
+
+local p = GetGlobalJsObj( "p" )
+print( p.x )
 
 )");
 
