f362a4d68e6ce4e1828924319789c265474b0d38
denghe <denghe@hotmail.com>
2023-08-01 17:27:07 +0800

    writing...

--- docs/set_cpp_func_to_js.cpp
+++ docs/set_cpp_func_to_js.cpp
@@ -27,21 +27,30 @@ void PrintVal(emscripten::val const& v) {
 // todo: replace int to shared ptr obj for destruct remove callback?
 
 int callbackAutoKey{};
-std::unordered_map<int, std::function<void()>> callbacks;
-void Callback(int key) {
-    callbacks[key]();
+std::unordered_map<int, std::function<emscripten::val(emscripten::val const&)>> callbacks;
+emscripten::val Callback(int key, emscripten::val const& a) {
+    return callbacks[key](a);
 }
 
 void SetMemberCallback(emscripten::val v, std::string fn) {
     callbackAutoKey++;
-    callbacks.insert(std::make_pair(callbackAutoKey, [k = callbackAutoKey]{
-        PrintStr((std::string("call back. key = ") + std::to_string(k)).c_str());
+    callbacks.insert(std::make_pair(callbackAutoKey, [k = callbackAutoKey](emscripten::val const& a)->emscripten::val {
+        auto s = std::string("cpp: callback. key = ") + std::to_string(k) + "\n";
+        PrintStr(s.c_str());
+        //PrintVal(a);
+        int r = 0;
+        if (a.isArray()) {
+            r = a["length"].as<int>();
+            for (int i = 0; i < r; ++i) {
+                PrintVal(a[i]);
+            }
+        }
+        return emscripten::val{r};
     }));
-    auto s = std::string("CppCallback = ()=>{ Module.Callback(" + std::to_string(callbackAutoKey) + "); };");
+    auto s = std::string("CppCallback = (...args)=>{ return Module.Callback(" + std::to_string(callbackAutoKey) + ", args); };");
     emscripten_run_script(s.c_str());
-    v.call<void>(fn.c_str(), emscripten::val::global("CppCallback"));
+    v.set(fn.c_str(), emscripten::val::global("CppCallback"));
 }
-
 
 EMSCRIPTEN_BINDINGS(my_module) {
     emscripten::function("Callback", &Callback);
